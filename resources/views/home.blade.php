9a670e625f4d10a646ea72f7e3322ffaa7827dc8
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-01-31 18:07:15 +0100

    #4 Remove unneccesary home view
    closes #4

--- resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <body class="antialiased">
-        <div>
-            <button><a href="{{url('/login')}}">Log me in</a></button>
-        </div>
-    </body>
-</html>
