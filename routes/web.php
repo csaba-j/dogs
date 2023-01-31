74b6aac2fa9906ae66b45c61d7958bf857ed22b4
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-01-31 18:05:26 +0100

    #4 Add Breeze package for auth and FE

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login', ['name' => "Csaba"]);
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
+require __DIR__.'/auth.php';
