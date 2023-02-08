c8ac52716b04246e91911475b668432615d9e72c
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-02-08 17:19:08 +0100

    Remove unused auth navigation links

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,17 +40,18 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <!-- <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
+-->
 
                 <x-button class="ml-3">
                     {{ __('Log in') }}
                 </x-button>
-            </div>
+            </div> 
         </form>
     </x-auth-card>
 </x-guest-layout>
