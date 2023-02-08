5c63f6b7bdacef409eba8ecd3a835f144f9b29c5
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-02-08 18:13:58 +0100

    Hotfix for edit: current image for dogapi objects

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -82,10 +82,8 @@
                                 <input type="file" name="image" id="image" value="{{ old('image') }}">
                                 <br>
                                 Current image:
-                                @if ($dog->reference_image_name != null)
-                                <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                                    <img src="{{asset('storage/'.$dog->reference_image_name)}}" class="h-full w-full object-cover object-center">
-                                </div>
+                                @if($dog->image['url'] != null)
+                                    <img src="{{ $dog->image['url'] }}"alt="A cute dog." class="h-full w-full object-cover object-center group-hover:opacity-75">   
                                 @else
                                     No image!
                                 @endif
