a6b3e5ca87c36f965368815c080e95427b2ced4e
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-02-08 18:16:53 +0100

    Hotfix: '#' link when there is no wiki link

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -33,7 +33,7 @@
                         <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                             @foreach($dogs as $dog)
                             <div class="relative">
-                                <a href="{{$dog->wikipedia_url}}" class="group">
+                                <a href="{{ $dog->wikipedia_url ? $dog->wikipedia_url : '#' }}" class="group">
                                     <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
                                         @if($dog->image['url'] != null)
                                             <img src="{{ $dog->image['url'] }}"alt="A cute dog." class="h-full w-full object-cover object-center group-hover:opacity-75">
